441b924aae091342b40eaecb9898abc075544b24
Oliver Eddy <oliver.eddy@developersinstitute.co.nz>
2024-07-03 15:18:22 +1200

    Conditionally render Navbar so it doesn't display on landing page

--- password-manager/src/App.js
+++ password-manager/src/App.js
@@ -1,47 +1,56 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import Home from './pages/Home';
 import Passwords from './pages/Passwords';
 import Account from './pages/Account';
 import PrivateRoute from './components/PrivateRoute';
-import NavBar from './components/NavBar'; // Import NavBar component
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <Router>
-      <div>
-        <NavBar /> {/* Render the NavBar component outside of the Routes */}
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route
-            path="/home"
-            element={
-              <PrivateRoute>
-                <Home />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/passwords"
-            element={
-              <PrivateRoute>
-                <Passwords />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/account"
-            element={
-              <PrivateRoute>
-                <Account />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
-      </div>
+      <Main />
     </Router>
+  );
+}
+
+function Main() {
+  const location = useLocation();
+  const hideNavBarPaths = ['/'];
+
+  return (
+    <div>
+      {!hideNavBarPaths.includes(location.pathname) && <NavBar />}
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route
+          path="/home"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/passwords"
+          element={
+            <PrivateRoute>
+              <Passwords />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/account"
+          element={
+            <PrivateRoute>
+              <Account />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </div>
   );
 }
 
